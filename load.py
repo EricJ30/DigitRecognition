c1e8a0200bee8ac430c224b56873b52ed474c720
Eric <ejiang26@uwo.ca>
2025-05-19 12:01:07 -0400

    feat: data loader and basic trainer

---
+++ load.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+import os
+import random
+
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
